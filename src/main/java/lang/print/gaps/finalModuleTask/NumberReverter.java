f285b862f9fb4f0b1769ad201b35fc306750ab2a
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-29 18:31:05 +0500

    final_module1

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        // abc  147
+        int a=number%10;
+        int b=(number%100-a)/10;
+        int c=(number-b*10-a)/100;
+
+        System.out.println(a+""+b+""+c);
     }
 }
