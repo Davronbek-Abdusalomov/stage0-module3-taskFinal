3b500a8dc552d36a970e264924dee321872e7f34
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-29 22:27:00 +0500

    final_module1

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -4,5 +4,6 @@ public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
         float fahrenheit = (float) (temperatureCelsius*1.8+32);
         System.out.println(fahrenheit);
+
     }
 }
