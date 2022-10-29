fe8d0c6bd58caf03923b2a6b3495dbba565ff0ab
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-29 22:14:18 +0500

    final_module1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int i=4;
+        while(i>0){
+            int remainder = number%10;
+            remainder+=remainder;
+            number=number/10;
+            i++;
+            System.out.println(remainder);
+        }
     }
 }
